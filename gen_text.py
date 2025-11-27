fe64cd5819431474a58cebe0ec03c3f63bb9fc58
RueiJia Huang <rueijiahuang@gmail.com>
2025-11-27 15:36:26 +0800

    change ALM to Audio Flamingo 3

--- gen_text.py
+++ gen_text.py
@@ -1,7 +1,7 @@
 import utils
 import librosa
 import os
-from transformers import AutoProcessor, Qwen2AudioForConditionalGeneration
+from transformers import AutoProcessor, Qwen2AudioForConditionalGeneration, AudioFlamingo3ForConditionalGeneration
 from transformers import BitsAndBytesConfig
 import torch
 from tqdm import tqdm
@@ -17,24 +17,26 @@ AUDIO_DIR = 'audio/'
 
 audio_files = os.listdir(AUDIO_DIR)
 audio_files = [os.path.join(AUDIO_DIR, a) for a in audio_files]
-print(audio_files[0])
 
-model = Qwen2AudioForConditionalGeneration.from_pretrained("Qwen/Qwen2-Audio-7B" ,
-                                                           trust_remote_code=True, 
-                                                           quantization_config=bnb_config,
-                                                           device_map='auto')
-processor = AutoProcessor.from_pretrained("Qwen/Qwen2-Audio-7B" ,trust_remote_code=True)
+# model = Qwen2AudioForConditionalGeneration.from_pretrained("Qwen/Qwen2-Audio-7B" ,
+#                                                            trust_remote_code=True, 
+#                                                            quantization_config=bnb_config,
+#                                                            device_map='auto')
+# processor = AutoProcessor.from_pretrained("Qwen/Qwen2-Audio-7B" ,trust_remote_code=True)
 
-print(model.device)
-# Example usage inferencing 1 audio
-prompt = "What do you hear from this music? Describe in terms of emotion and atmosphere"
+model = AudioFlamingo3ForConditionalGeneration.from_pretrained("nvidia/audio-flamingo-3-hf" ,
+                                                            quantization_config=bnb_config,
+                                                            device_map='auto')
+processor = AutoProcessor.from_pretrained("nvidia/audio-flamingo-3-hf")
+
+prompt = "Give a 5 to 10 labels for the music in terms of emotion, atmosphere and vibes"
 
 messages = [
     {
         "role": "user",
         "content": [
-            {"type": "audio"},
-            {"type": "text", "text": prompt}
+            {"type": "text", "text": prompt},
+            {"type": "audio", "path": ''}
         ]
     }
 ]
@@ -42,19 +44,21 @@ messages = [
 result = []
 for file in tqdm(audio_files, desc='Audio files'):
     try:
-        text = processor.apply_chat_template(messages, add_generation_prompt=True, tokenize=False)
-
-        audio, sr = librosa.load(file, sr=processor.feature_extractor.sampling_rate)
-        audio = audio[:30*sr]
-        inputs = processor(text=text, audio=audio, return_tensors="pt")
-        inputs = {k: v.to(model.device) for k, v in inputs.items()}
-        
-        generated_ids = model.generate(**inputs, max_length=4096)
+        messages[0]["content"][1]["path"] = file
+        inputs = processor.apply_chat_template(
+            messages,
+            tokenize=True,
+            add_generation_prompt=True,
+            return_dict=True,
+        ).to(model.device, dtype=torch.float16)   
+            
+        generated_ids = model.generate(**inputs)
         generated_ids = generated_ids[:, inputs['input_ids'].size(1):]
         response = processor.batch_decode(generated_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+        print(os.path.basename(file), '\n', response)
         result.append({'song name': os.path.basename(file), 'description': response})
     except Exception as e:
         print(f"Failed handling {file}: {e}")
 df = pd.DataFrame(result)
-df.to_csv("audio_summaries.csv", index=True)
+df.to_csv("description_AF3.csv", index=True)
 print("Done")
