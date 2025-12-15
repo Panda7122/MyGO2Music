1b77ab2326aaffad9bd3927ead637838fb1eb48e
RueiJia Huang <rueijiahuang@gmail.com>
2025-12-15 12:27:41 +0800

    Batch-wise text embedding to handle large dataset

--- CLIP_cosine_sim.py
+++ CLIP_cosine_sim.py
@@ -3,17 +3,20 @@ import torch
 import pandas as pd
 from PIL import Image
 from tqdm import tqdm
-from transformers import CLIPProcessor, CLIPModel, CLIPConfig
+from transformers import CLIPProcessor, CLIPModel, CLIPConfig, CLIPTextModelWithProjection, AutoTokenizer
 
 IMAGE_FOLDER = "mygo_image"
-CSV_FILE = "description_fma_small_AF3.csv"
+CSV_FILE = "description_AF3.csv"
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+BATCH_SIZE = 100
 #dtype = 
 
 # Load Long CLIP
 model_id = ("zer0int/LongCLIP-GmP-ViT-L-14")
 config = CLIPConfig.from_pretrained(model_id)
 config.text_config.max_position_embeddings = 248
+text_emb_model = CLIPTextModelWithProjection.from_pretrained(model_id, config=config.text_config).to(DEVICE)
+tokenizer = AutoTokenizer.from_pretrained(model_id)
 model = CLIPModel.from_pretrained(model_id, config=config).to(DEVICE)
 processor = CLIPProcessor.from_pretrained(model_id, padding="max_length", max_length=248)
 
@@ -24,6 +27,20 @@ songs = df["song name"].tolist()
 descriptions = df["description"].tolist()
 
 results = []
+text_embs = []
+
+# Get text embeddings
+for i in range(0, len(descriptions), BATCH_SIZE):
+    batch_descriptions = descriptions[i:i + BATCH_SIZE]
+    inputs = tokenizer(batch_descriptions, return_tensors="pt", padding="max_length", truncation=True).to(DEVICE)
+    outputs = text_emb_model(**inputs)
+    batch_text_embs = outputs.text_embeds
+    batch_text_embs /= batch_text_embs.norm(dim=-1, keepdim=True)
+    text_embs.append(batch_text_embs)
+    
+    
+text_embs = torch.cat(text_embs, dim=0).to(DEVICE)
+
 
 # Process each image
 for image_file in tqdm(os.listdir(IMAGE_FOLDER)):
@@ -32,23 +49,27 @@ for image_file in tqdm(os.listdir(IMAGE_FOLDER)):
 
     image_path = os.path.join(IMAGE_FOLDER, image_file)
     image = Image.open(image_path).convert("RGB")
-
-    inputs = processor(text=descriptions, images=image, return_tensors="pt", padding="max_length", truncation=True).to(DEVICE)
-    outputs = model(**inputs)
-    logits_per_image = outputs.logits_per_image # this is the image-text similarity score
-    
-    top_k = torch.topk(logits_per_image, 3)
-    bottom_k = torch.topk(logits_per_image, 3, largest=False)
+    with torch.no_grad():
+        inputs = processor(images=image, return_tensors="pt").to(DEVICE)
+        outputs = model.get_image_features(**inputs)
+        outputs /= outputs.norm(dim=-1, keepdim=True)
+        
+        logits_per_image = (outputs @ text_embs.T).squeeze(0)
+        
+        top_k = torch.topk(logits_per_image, 3)
+        bottom_k = torch.topk(logits_per_image, 3, largest=False)
     
     results.append({
         "image": image_file,
-        "top_3_songs": [songs[i] for i in top_k.indices[0].cpu()],
-        "top_3_scores": [round(float(s), 4) for s in top_k.values[0].cpu()],
-        "bottom_3_songs": [songs[i] for i in bottom_k.indices[0].cpu()],
-        "bottom_3_scores": [round(float(s), 4) for s in bottom_k.values[0].cpu()]
+        "top_3_songs": [songs[int(i)] for i in top_k.indices.cpu().detach()],
+        "top_3_scores": [round(float(s), 4) for s in top_k.values.cpu().detach()],
+        "bottom_3_songs": [songs[int(i)] for i in bottom_k.indices.cpu().detach()],
+        "bottom_3_scores": [round(float(s), 4) for s in bottom_k.values.cpu().detach()]
     })
+    del inputs, outputs, logits_per_image, top_k, bottom_k
+    torch.cuda.empty_cache()
 
 # Save results
 out_df = pd.DataFrame(results)
-out_df.to_csv("clip_similarity_results_fma_small.csv", index=False)
+out_df.to_csv("clip_similarity_results_fma.csv", index=False)
 
